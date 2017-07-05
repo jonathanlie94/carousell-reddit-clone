a81cf91608dc12300ec56f17c26682937e73f656
jonathanlie94 <jonathanlie94@gmail.com>
2017-07-06 01:40:51 +0800

    Add fontSizes to ui theme

--- src/ui/theme.js
+++ src/ui/theme.js
@@ -5,4 +5,11 @@ export default {
     grey: '#C8C8C8',
     blue: '#00C3FF',
   },
+  fontSizes: {
+    small: '0.75rem',
+    regularSm: '0.875rem',
+    regular: '1rem',
+    regularL: '1.25rem',
+    large: '1.5rem',
+  },
 };
