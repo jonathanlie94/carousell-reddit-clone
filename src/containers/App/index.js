ca4839457e8fa8b66b21957d86e1a231f0499b26
jonathanlie94 <jonathanlie94@gmail.com>
2017-07-05 20:05:37 +0800

    Move meta tags to App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import asyncComponent from 'components/AsyncComponent';
 import { Banner, Header, MainContainer, SideContainer } from 'components';
 import styled from 'styled-components';
+import Helmet from 'react-helmet';
 const TopicsPage = asyncComponent(() => import('containers/TopicsPage'));
 const HomePage = asyncComponent(() => import('containers/HomePage'));
 const SubmitPage = asyncComponent(() => import('containers/SubmitPage'));
@@ -16,6 +17,14 @@ const ContentWrapper = styled.div`
 
 const App = props =>
   <Wrapper>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <title>Reddit Clone</title>
+    </Helmet>
+
     <Header>
       <Banner />
     </Header>
