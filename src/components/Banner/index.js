0351d0c81b9a7068177b951b9144f023d792cb2c
Jonathan Lie <jonathanlie94@gmail.com>
2017-07-06 16:25:53 +0800

    Add title text to Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -10,13 +10,35 @@ const Wrapper = styled.div`
   color: ${theme.colors.grey};
   width: 100%;
   height: 250px;
+  position: relative;
 `;
+
+const TitleText = styled.span`
+  background-color: transparent;
+  font-family: Overwatch;
+  font-style: italic;
+  text-decoration: none;
+  text-shadow: 0 1px 1px;
+  color: ${theme.colors.grey};
+  font-size: 6rem;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  margin-left: 2rem;
+`;
+
+const ColoredTitleInnerText = styled.span`color: ${theme.colors.darkergrey};`;
 
 class Banner extends Component {
   render() {
     return (
       <Link to="/">
         <Wrapper image="/banner.jpg">
+          <TitleText>
+            REDDIT
+            <ColoredTitleInnerText>CLONE</ColoredTitleInnerText>
+          </TitleText>
+
           {this.props.children}
         </Wrapper>
       </Link>
