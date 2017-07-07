78ebc15e0e84a78b991406a363c491aed1a9bf46
jonathanlie94 <jonathanlie94@gmail.com>
2017-07-07 09:12:41 +0800

    Update imports

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as AsyncComponent } from './AsyncComponent';
 export { default as Banner } from './Banner';
 export { default as Button } from './Button';
 export { default as Header } from './Header';
+export { default as InputText } from './InputText';
 export { default as ListItem } from './ListItem';
 export { default as ListView } from './ListView';
 export { default as MainContainer } from './MainContainer';
