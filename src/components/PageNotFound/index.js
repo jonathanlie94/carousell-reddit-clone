0e4904fbf75566157655ea7d6f95b596a49e9918
jonathanlie94 <jonathanlie94@gmail.com>
2017-07-09 01:03:24 +0800

    Refactor page not found layout and add i18n

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -5,12 +5,25 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { theme } from 'ui';
+import messages from './messages';
+import { FormattedMessage } from 'react-intl';
 
-const Wrapper = styled.div`color: ${theme.colors.blue};`;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8rem 0;
+  font-size: ${theme.fontSizes.largeXX};
+  color: ${theme.colors.darkergrey};
+`;
 
 class PageNotFound extends Component {
   render() {
-    return <Wrapper>Page Not Found</Wrapper>;
+    return (
+      <Wrapper>
+        <FormattedMessage {...messages.text} />
+      </Wrapper>
+    );
   }
 }
 
