c432d614f395d99795f777e34c24963c95ee4755
jonathanlie94 <jonathanlie94@gmail.com>
2017-07-08 23:41:01 +0800

    Fix server.js to redirect all requests to /

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/build/'));
 
+app.get('/*', function(req, res){
+  res.sendFile(__dirname + '/build/index.html');
+});
+
 app.listen(process.env.PORT || 8080);
