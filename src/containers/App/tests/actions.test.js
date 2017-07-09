019ec89ff62934900a2b87730a32d6985aa50f93
jonathanlie94 <jonathanlie94@gmail.com>
2017-07-09 14:44:11 +0800

    Write tests for App reducers and actions

---
+++ src/containers/App/tests/actions.test.js
@@ -0,0 +1,95 @@
+import * as actions from '../actions';
+import * as constants from '../constants';
+import { fromJS } from 'immutable';
+import * as sampleDataManager from 'utils/sampleDataManager';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('actions', () => {
+  it('should create an action to load topics', () => {
+    const expectedAction = {
+      type: constants.LOAD_TOPICS,
+    };
+    expect(actions.loadTopics()).toEqual(expectedAction);
+  });
+
+  it('creates LOAD_TOPICS_SUCCESS after fetching is done', () => {
+    const data = fromJS(sampleDataManager.getDataList(1, 20));
+    const expectedActions = [
+      { type: constants.LOAD_TOPICS },
+      {
+        type: constants.LOAD_TOPICS_SUCCESS,
+        topics: data.get('topics'),
+        meta: data.get('meta'),
+      },
+    ];
+    const store = mockStore({ topics: {} });
+
+    return store.dispatch(actions.requestFetchTopics()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('should create an action to load a topic', () => {
+    const expectedAction = {
+      type: constants.LOAD_TOPIC,
+    };
+    expect(actions.loadTopic()).toEqual(expectedAction);
+  });
+
+  it('creates LOAD_TOPIC_SUCCESS after fetching is done', () => {
+    const data = fromJS(sampleDataManager.getData(1));
+    const expectedActions = [
+      { type: constants.LOAD_TOPIC },
+      {
+        type: constants.LOAD_TOPIC_SUCCESS,
+        topic: data,
+      },
+    ];
+    const store = mockStore({ topics: {} });
+
+    return store.dispatch(actions.requestFetchTopic(1)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('creates UPVOTE_TOPIC after upvote', () => {
+    const data = fromJS(sampleDataManager.upvote(1));
+    const expectedActions = [
+      {
+        type: constants.UPVOTE_TOPIC,
+        topic: data,
+      },
+    ];
+    // Downvote it first to return the value back before upvoting it back again,
+    // as the sampleDataManager updates the in-memory data immediately.
+    sampleDataManager.downvote(1);
+
+    const store = mockStore({ topics: {} });
+
+    return store.dispatch(actions.requestUpvoteTopic(1)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('creates DOWNVOTE_TOPIC after downvote', () => {
+    const data = fromJS(sampleDataManager.downvote(1));
+    const expectedActions = [
+      {
+        type: constants.DOWNVOTE_TOPIC,
+        topic: data,
+      },
+    ];
+    // Upvote it first to return the value back before downvoting it back again,
+    // as the sampleDataManager updates the in-memory data immediately.
+    sampleDataManager.upvote(1);
+    const store = mockStore({ topics: {} });
+
+    return store.dispatch(actions.requestDownvoteTopic(1)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
---
+++ src/containers/App/tests/reducers.test.js
@@ -0,0 +1,203 @@
+import reducer from '../reducers';
+import * as constants from '../constants';
+import { fromJS } from 'immutable';
+import * as sampleDataManager from 'utils/sampleDataManager';
+
+describe('appReducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(
+      fromJS({
+        loading: false,
+        error: false,
+        topics: {},
+      }),
+    );
+  });
+
+  it('should handle LOAD_TOPICS', () => {
+    expect(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+        }),
+        {
+          type: constants.LOAD_TOPICS,
+        }
+      )
+    ).toEqual(
+      reducer(
+        fromJS({
+          loading: true,
+          error: false,
+        }),
+        {
+          type: constants.LOAD_TOPICS,
+        }
+      )
+    );
+  });
+
+  it('should handle LOAD_TOPIC', () => {
+    expect(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+        }),
+        {
+          type: constants.LOAD_TOPICS,
+        }
+      )
+    ).toEqual(
+      reducer(
+        fromJS({
+          loading: true,
+          error: false,
+        }),
+        {}
+      )
+    );
+  });
+
+  it('should handle LOAD_TOPICS_SUCCESS', () => {
+    const data = sampleDataManager.getDataList(1, 20);
+
+    expect(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {},
+        }),
+        {
+          type: constants.LOAD_TOPICS_SUCCESS,
+          topics: fromJS(data),
+        }
+      )
+    ).toEqual(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: data,
+        }),
+        {}
+      )
+    );
+  });
+
+  it('should handle UPVOTE_TOPIC', () => {
+    const data = fromJS(sampleDataManager.upvote(1));
+
+    expect(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {},
+        }),
+        {
+          type: constants.UPVOTE_TOPIC,
+          topic: data,
+        }
+      )
+    ).toEqual(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {
+            1: data,
+          },
+        }),
+        {}
+      )
+    );
+  });
+
+  it('should handle DOWNVOTE_TOPIC', () => {
+    const data = fromJS(sampleDataManager.downvote(1));
+
+    expect(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {},
+        }),
+        {
+          type: constants.DOWNVOTE_TOPIC,
+          topic: data,
+        }
+      )
+    ).toEqual(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {
+            1: data,
+          },
+        }),
+        {}
+      )
+    );
+  });
+
+  it('should handle LOAD_TOPIC_SUCCESS', () => {
+    const data = fromJS(sampleDataManager.getData(1));
+
+    expect(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {},
+        }),
+        {
+          type: constants.LOAD_TOPIC_SUCCESS,
+          topic: data,
+        }
+      )
+    ).toEqual(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {
+            1: data,
+          },
+        }),
+        {}
+      )
+    );
+  });
+
+  it('should handle LOAD_TOPIC_ERROR', () => {
+    const data = sampleDataManager.getData(21);
+
+    expect(
+      reducer(
+        fromJS({
+          loading: false,
+          error: false,
+          topics: {},
+        }),
+        {
+          type: constants.LOAD_TOPIC_ERROR,
+          error: data,
+        }
+      )
+    ).toEqual(
+      reducer(
+        fromJS({
+          loading: false,
+          error: data,
+          topics: {},
+        }),
+        {}
+      )
+    );
+  });
+});
