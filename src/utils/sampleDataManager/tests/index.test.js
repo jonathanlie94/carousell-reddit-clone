7b6d7c8884f181f064989137fdf4fbf8e0d22266
jonathanlie94 <jonathanlie94@gmail.com>
2017-07-09 23:51:06 +0800

    Update sampleDataManager tests

--- src/utils/sampleDataManager/tests/index.test.js
+++ src/utils/sampleDataManager/tests/index.test.js
@@ -3,42 +3,33 @@ import * as sampleDataManager from '../';
 describe('sampleDataManager', () => {
   it('should be able to get a single default data', () => {
     expect(sampleDataManager.getData(1).id).toEqual(1);
-    expect(sampleDataManager.getData(1).title).toEqual(
-      'Today teaser date showed up in Orisa trailer.'
-    );
-    expect(sampleDataManager.getData(1).votes).toEqual(583);
-    expect(sampleDataManager.getData(1).description).toEqual(
-      '7 on the screen, 4 on the toolbox below. I might be reaching but I BELIEVE THIS.'
-    );
   });
 
   it('should be able to get default data list', () => {
     expect(Object.keys(sampleDataManager.getDataList(1).topics).length).toEqual(
       20
     );
-    expect(sampleDataManager.getDataList(1).meta.total).toEqual(41);
   });
 
   it('should be able to add new item', () => {
+    const total = sampleDataManager.getDataList(1).meta.total;
     sampleDataManager.create({ title: 'Title 1', description: '' });
-    expect(sampleDataManager.getDataList(1).meta.total).toEqual(42);
+    expect(sampleDataManager.getDataList(1).meta.total).toEqual(total + 1);
   });
 
   it('should be able to get last page after adding new item', () => {
+    const meta = sampleDataManager.getDataList(1).meta;
     sampleDataManager.create({ title: 'Title 2', description: '' });
-    expect(Object.keys(sampleDataManager.getDataList(1).topics).length).toEqual(
-      20
-    );
-    expect(Object.keys(sampleDataManager.getDataList(2).topics).length).toEqual(
-      20
-    );
-    expect(Object.keys(sampleDataManager.getDataList(3).topics).length).toEqual(
-      3
-    );
+    expect(
+      Object.keys(
+        sampleDataManager.getDataList(Math.ceil(meta.total / meta.per_page))
+          .topics
+      ).length
+    ).toEqual(meta.total - 2 * meta.per_page + 1);
   });
 
   it('should reject if title is more than 255 characters', () => {
-    expect(sampleDataManager.getDataList(1).meta.total).toEqual(43);
+    const total = sampleDataManager.getDataList(1).meta.total;
     expect(
       sampleDataManager.create({
         title:
@@ -46,7 +37,7 @@ describe('sampleDataManager', () => {
         description: '',
       }).title
     ).toEqual('Title is too long!');
-    expect(sampleDataManager.getDataList(1).meta.total).toEqual(43);
+    expect(sampleDataManager.getDataList(1).meta.total).toEqual(total);
   });
 
   it('should be able to upvote correctly', () => {
